1d50f6b00a4235713512b3d2ee7f3c40b269dda5
mrv90 <hamid.mousavinasab@gmail.com>
2019-12-13 18:30:04 +0330

    تسک تبدیل انواع تایپها به یکدیگر و بررسی آنها

---
+++ CppProject/0512.TypeConversion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	// practice of converting all type of primitive type, goes here!
+	// TODO int, float, double, const char*, const wchar_t*, std::string, std::wstring, 
+	std::string a1 = std::string("");
+	auto a2 = "";
+	// TODO Complex consists of real and imaginary parts
+}
