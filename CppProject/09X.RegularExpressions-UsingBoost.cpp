c38df2fbff516249e987797933aeed4e5d6205f7
mrv90 <hamid.mousavinasab@gmail.com>
2019-12-24 12:29:17 +0330

    باتوجه به استفاده از itrators در boost::regex یه سری از خطوط اضافی بودند؛ که بازتولید شدند.

--- CppProject/09X.RegularExpressions-UsingBoost.cpp
+++ CppProject/09X.RegularExpressions-UsingBoost.cpp
@@ -1,40 +1,35 @@
-﻿//#include <iostream>
-//#include <boost/regex.hpp>
-//
-//int main() {
-//	std::string url = "/class?class_id=-1&course_ref=1&student_ref=2&score_ref=1";
-//	
-//	const boost::regex queries_pattern("[a-zA-Z0-9_=-]+((?=&)|(?=$))");
-//	boost::smatch queries_result;;
-//	boost::regex_search(url, queries_result, queries_pattern);
-//
-//	std::string results("");
-//	
-//	boost::sregex_token_iterator iter(url.begin(), url.end(), queries_pattern, 0);
-//	boost::sregex_token_iterator end;
-//	
-//	for (; iter != end; ++iter) {
-//		std::cout << *iter << '\n';
-//	}	
-//	
-//	std::cin.get();
-//}
-
+﻿#include <iostream>
 #include <boost/regex.hpp>
-#include <iostream>
-#include <string>
 
 int main() {
-	std::string text("abc abd");
-	boost::regex regex("ab.");
-
-	boost::sregex_token_iterator iter(text.begin(), text.end(), regex, 0);
+	std::string url = "/class?class_id=-1&course_ref=1&student_ref=2&score_ref=1";
+	
+	const boost::regex pattern("[a-zA-Z0-9_=-]+((?=&)|(?=$))");
+	boost::sregex_token_iterator iter(url.begin(), url.end(), pattern, 0);
 	boost::sregex_token_iterator end;
-
+	
 	for (; iter != end; ++iter) {
 		std::cout << *iter << '\n';
-	}
-
+	}	
+	
 	std::cin.get();
-	return 0;
 }
+
+//#include <boost/regex.hpp>
+//#include <iostream>
+//#include <string>
+//
+//int main() {
+//	std::string text("abc abd");
+//	boost::regex regex("ab.");
+//
+//	boost::sregex_token_iterator iter(text.begin(), text.end(), regex, 0);
+//	boost::sregex_token_iterator end;
+//
+//	for (; iter != end; ++iter) {
+//		std::cout << *iter << '\n';
+//	}
+//
+//	std::cin.get();
+//	return 0;
+//}
