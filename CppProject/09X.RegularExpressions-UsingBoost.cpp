50ce6bb79da2784efdf451262f709e320ce24579
mrv90 <hamid.mousavinasab@gmail.com>
2020-01-04 20:28:25 +0330

    پیمایش همزمان یک رشته و برداشتن مقادیر کلید و مقدار از آن

--- CppProject/09X.RegularExpressions-UsingBoost.cpp
+++ CppProject/09X.RegularExpressions-UsingBoost.cpp
@@ -1,17 +1,38 @@
 ﻿#include <iostream>
 #include <boost/regex.hpp>
 
+//int main() {
+//	std::string url = "/class?class_id=-1&course_ref=1&student_ref=2&score_ref=1";
+//	
+//	const boost::regex pattern("[a-zA-Z0-9_=-]+((?=&)|(?=$))");
+//	boost::sregex_token_iterator iter(url.begin(), url.end(), pattern, 0);
+//	boost::sregex_token_iterator end;
+//	
+//	for (; iter != end; ++iter) {
+//		std::cout << *iter << '\n';
+//	}	
+//	
+//	std::cin.get();
+//}
+
 int main() {
-	std::string url = "/class?class_id=-1&course_ref=1&student_ref=2&score_ref=1";
-	
-	const boost::regex pattern("[a-zA-Z0-9_=-]+((?=&)|(?=$))");
-	boost::sregex_token_iterator iter(url.begin(), url.end(), pattern, 0);
+	std::string body = "{\"birth_date\":\"e3\",\"family_name\":\"e2\",\"name\":\"e1\",\"student_id\":\"1\"}";
+
+	const boost::regex key("[a-zA-Z0-9_]+(?=\":)");
+	const boost::regex val("[a-zA-Z0-9_]+(?=(\",|\"}|}))");
+	boost::sregex_token_iterator key_it(body.begin(), body.end(), key, 0);
+	boost::sregex_token_iterator val_it(body.begin(), body.end(), val, 0);
 	boost::sregex_token_iterator end;
-	
-	for (; iter != end; ++iter) {
-		std::cout << *iter << '\n';
-	}	
-	
+
+	std::string s = "";
+	for (; key_it != end && val_it != end; ) {
+		s.append(!s.empty() ? (", " + *key_it + "='" + *val_it + "'") : (*key_it + "='" + *val_it + "'"));
+		
+		++key_it;
+		++val_it;
+	}
+
+	std::cout << s << std::endl;
 	std::cin.get();
 }
 
